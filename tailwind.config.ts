5f4704bf18f8439f32eed8b72f71b2c8398d8e24
rajib217 <rajib.hasan3@gmail.com>
2025-08-04 18:52:11 +0600

    Font Update
    Noto Sans

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ export default {
 			}
 		},
 		extend: {
+			fontFamily: {
+				sans: ["Noto Sans Bengali", "sans-serif"],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
