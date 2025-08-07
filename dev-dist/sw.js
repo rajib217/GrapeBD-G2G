7dee5d633d965de04983964ae0bab0ed2eda9635
rajib217 <rajib.hasan3@gmail.com>
2025-08-07 11:09:22 +0600

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0vkurnhfsqo"
+    "revision": "0.3usmjjvvk2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
